dbc7c1d7b9997916030458f62ef42404f6136e85
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-02-27 12:43:02 +0100

    Deploy the KNN model with django.

---
+++ house_price/src/knn_model.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import pickle
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('../../components_machine_learning/house_cleaned_datas.csv')
+
+
+# Features
+numeric_features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'sqft_living15', 'sqft_lot15', 'floors', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated', 'grade', 'view', 'waterfront', 'condition']
+categorical_features = ['zipcode']
+
+
+# Transformers
+numeric_transformer = StandardScaler()
+categorical_transformer = OneHotEncoder()
+
+# Pre-processing
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features),
+    ])
+
+# Définition du Pipeline
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('regressor', KNeighborsRegressor(n_neighbors=5))])
+
+X = df.drop(['price', 'lat', 'long', 'date'], axis=1)
+y = df['price']
+
+# Entraînement du modèle
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# On laisse une parti du pipeline en entraînement
+pipeline.fit(X_train, y_train)
+
+# On passe l'étape de la prédiction aux tests
+y_pred = pipeline.predict(X_test)
+
+with open('knn_model.pkl', 'wb') as f:
+    pickle.dump(pipeline, f)
+
--- house_price/views.py
+++ house_price/views.py
@@ -4,9 +4,9 @@ from .forms import EstimateForm
 import pickle
 import pandas as pd
 
-# Charger le modèle entraîné
-with open('model.pkl', 'rb') as file:
-    pipeline = pickle.load(file)
+def estimation(request):
+    form = EstimateForm()
+    return render(request, 'estimation.html', {'form': form})
 
 def estimate(request):
     if request.method == 'POST':
@@ -22,6 +22,15 @@ def estimate(request):
             sqft_basement = form.cleaned_data['sqft_basement']
             yr_built = form.cleaned_data['yr_built']
             yr_renovated = form.cleaned_data['yr_renovated']
+            grade = form.cleaned_data['grade']
+            view = form.cleaned_data['view']
+            waterfront = form.cleaned_data['waterfront']
+            condition = form.cleaned_data['condition']
+
+
+            # Charger le modèle entraîné
+            pickle_in = open('house_price/src/knn_model.pkl', 'rb')
+            pipeline = pickle.load(pickle_in)
 
             data = pd.DataFrame({
                 'zipcode': [zipcode],
@@ -34,12 +43,19 @@ def estimate(request):
                 'sqft_basement': [sqft_basement],
                 'yr_built': [yr_built],
                 'yr_renovated': [yr_renovated],
+                'sqft_living15': [2000],
+                'sqft_lot15': [8000],
+                'grade': [7],
+                'view': [0],
+                'waterfront': [0],
+                'condition': [3],
             })
 
+            # Utilisation du modèle pour faire la prédiction
             prediction = pipeline.predict(data)
 
-            return render(request, 'result.html', {'prediction': prediction[0]})
+        return render(request, 'test.html', {'prediction': prediction[0]})
     else:
         form = EstimateForm()
 
-    return render(request, 'estimate.html', {'form': form})
+        return render(request, 'estimation.html', {'form': form})
