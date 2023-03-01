4bda2772554394a2f48c909105a405fa13749048
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-03-01 09:07:44 +0100

    Update KNN model pickle and Dataframe.

--- house_price/src/knn_model.py
+++ house_price/src/knn_model.py
@@ -35,6 +35,8 @@ y = df['price']
 # Entraînement du modèle
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+preprocessor.fit_transform(X)
+
 # On laisse une parti du pipeline en entraînement
 pipeline.fit(X_train, y_train)
 
